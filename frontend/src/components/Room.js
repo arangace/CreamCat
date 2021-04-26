f74cc73a2a4f186a5ae8d8af33bf0bd0934cb0f5
sun <sjun521@aucklanduni.ac.nz>
2021-04-27 02:15:51 +1200

    <feat> :  added a search bar that can search videos on youtube, returning the first 5 results

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,11 +1,13 @@
 import "react-bootstrap";
 import {useContext} from 'react';
 import {AppContext} from '../AppContextProvider';
+import SearchBar from './SearchBar'
 
 export default function Room() {
     const { roomID, name, description } = useContext(AppContext);
-    return (
+    return(
         <>
+            <SearchBar/>
             <div>{roomID}</div>
             <div>{name}</div>
             <div>{description}</div>
