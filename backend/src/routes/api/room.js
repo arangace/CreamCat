dfb5968d2904c18b92701800d46c97513e331a62
Akunda <397062449@qq.com>
2021-04-28 16:56:43 +0800

    fix: room backend error handling, fix #26

--- backend/src/routes/api/room.js
+++ backend/src/routes/api/room.js
@@ -9,58 +9,85 @@ const HTTP_BAD_REQUEST = 400;
 const router = express.Router();
 
 router.post('/create/', async (req, res) => {
-    const room = {
-        name: req.body.name,
-        description: req.body.description,
-        password: req.body.password
-    };
+    try{
+        const room = {
+            name: req.body.name,
+            description: req.body.description,
+            password: req.body.password
+        };
 
-    if(room.name){
-        const newRoom = await createRoom(room);
-        res.status(HTTP_CREATED)
-        .header('Location', `/${newRoom._id}`)
-        .json(newRoom);
-    }
-    else{
+        if(room.name){
+            const newRoom = await createRoom(room);
+            res.status(HTTP_CREATED)
+            .header('Location', `/${newRoom._id}`)
+            .json(newRoom);
+        }
+        else{
+            res.json('Room name is required!');
+        }
+    }catch{
         res.sendStatus(HTTP_BAD_REQUEST);
     }
 });
 
 router.post('/join/', async (req, res) => {
-    const id = req.body._id;
-    const password = req.body.password;
+    try{
+        const id = req.body._id;
+        const password = req.body.password;
 
-    console.log(`Join room requested for ${id}`)
-
-    const room = await retrieveRoom(id);
-    if(room){
-        if(room.password){
-            if(room.password == password){
-                res.header('Location', `/${room._id}`)
-            .json(room);
+        console.log(`Join room requested for ${id}`)
+        if(id){
+            const room = await retrieveRoom(id);
+            if(room){
+                if(room.password){
+                    if(room.password == password){
+                        res.header('Location', `/${room._id}`)
+                    .json(room);
+                    }
+                    else{
+                        res.json('Password incorrect!');
+                        console.log()
+                    }
+                }
+                else{
+                    res.header('Location', `/${room._id}`)
+                    .json(room);
+                }
             }
             else{
-                res.json('password incorrect!');
-                console.log()
+                res.json('Room not found!');
             }
         }
         else{
-            res.header('Location', `/${room._id}`)
-            .json(room);
+            res.json('Room ID is required!')
         }
-    }
-    else{
-        res.json('room not found!');
+    }catch{
+        res.sendStatus(HTTP_BAD_REQUEST);
     }
 });
 
 router.put('/:id', async (req, res) => {
     try{
-        const { id } = req.params;
-        const room = req.body;
-        room._id = id;
-        const success = await updateRoom(room);
-        res.sendStatus(success ? HTTP_NO_CONTENT : HTTP_NOT_FOUND);
+        const dbroom = await retrieveRoom(req.params);
+        if(dbroom){
+            if(room.password == req.body.oldpassword){
+                const { id } = req.params;
+                const room = {
+                    name: req.body.name,
+                    description: req.body.description,
+                    password: req.body.password
+                };
+                room._id = id;
+                const success = await updateRoom(room);
+                res.sendStatus(success ? HTTP_NO_CONTENT : HTTP_NOT_FOUND);
+            }
+            else{
+                res.json('Password incorrect!');
+            }
+        }
+        else{
+            res.json('Room not found!');
+        }
     }catch{
         res.sendStatus(HTTP_BAD_REQUEST);
     }
@@ -68,9 +95,19 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
     try{
-        const { id } = req.params;
-        await deleteRoom(id);
-        res.sendStatus(HTTP_NO_CONTENT);
+        const room = await retrieveRoom(req.params);
+        if(room){
+            if(room.password == req.body.password){
+                await deleteRoom(id);
+                res.sendStatus(HTTP_NO_CONTENT);
+            }
+            else{
+                res.json('Password incorrect!');
+            }
+        }
+        else{
+            res.json('Room not found!');
+        }
     }catch{
         res.sendStatus(HTTP_BAD_REQUEST);
     }  
