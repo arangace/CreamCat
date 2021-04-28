f0cd7a8c8444ee619ac6f11be402e78cbfd74501
Akunda <397062449@qq.com>
2021-04-28 16:20:47 +0800

    fix: mongodb warning, fix #25

--- backend/src/rooms-data/db-connect.js
+++ backend/src/rooms-data/db-connect.js
@@ -4,6 +4,7 @@ const DEFAULT_CONNECTION_STRING = 'mongodb://localhost:27017/creamcat';
 
 export default function connectToDatabase(connectionString = DEFAULT_CONNECTION_STRING) {
     return mongoose.connect(connectionString, {
-        useNewUrlParser: true
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     });
 }
