3b33f580d3d283d0da88daf2d6c6421f9c5df7f7
Akunda <397062449@qq.com>
2021-05-01 09:59:38 +0800

    fix: add error handling

--- backend/src/socket.io/socket-api.js
+++ backend/src/socket.io/socket-api.js
@@ -3,29 +3,34 @@ import { retrieveRoom } from "../rooms-data/rooms-dao";
 
 
 export default async function onConnection(socket, io) {
-    const { roomID, password } = socket.handshake.query;
-    const room = await retrieveRoom(roomID);
-    console.log('a user connected');
-    if(room){
-        if(room.password == password){
-            socket.join(roomID);
-            console.log(`user joined` + roomID);
+    try{
+        const { roomID, password } = socket.handshake.query;
+        const room = await retrieveRoom(roomID);
+        console.log('a user connected');
+        if(room){
+            if(room.password == password){
+                socket.join(roomID);
+                console.log(`user joined` + roomID);
+            }
+            else{
+
+            }
         }
         else{
 
         }
-    }
-    else{
+        socket.on('update', () =>{
+            console.log("update");
+            io.to(roomID).emit('update');
+        });
+    
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+          });
+    }catch{
 
     }
 
-    socket.on('update', () =>{
-        console.log("update");
-        io.to(roomID).emit('update');
-    });
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-      });
+    
 
 }
