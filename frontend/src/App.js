df57fb24fea3b391ba70d4a7b38244a90730c9e9
Akunda <397062449@qq.com>
2021-05-06 11:58:25 +0800

    fix: handle when room in localstorage does not exist in the database

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,30 @@ import HomePage from "./components/HomePage";
 import JoinRoomPage from "./components/JoinRoom";
 import NavBar from "./components/Navbar";
 import RoomPage from "./components/RoomPage";
+import axios from 'axios';
+import { useContext } from "react";
+import { AppContext } from "./AppContextProvider";
 
 function App() {
+
+    const { setCurrentRoom } = useContext(AppContext);
+
+    async function checkRoomExist(){
+        const localStorageCurrentRoom = JSON.parse(localStorage.getItem("currentRoom"));
+        if(localStorageCurrentRoom){
+            const response = await axios.post(
+                "http://localhost:3000/api/room/join/",
+                localStorageCurrentRoom
+            );
+            if(!response.data.name){
+                localStorage.removeItem("currentRoom");
+                setCurrentRoom("");
+            }
+        }
+    }
+    checkRoomExist();
+
+
     return (
         <div>
             <div>
