9dedf86a9e4decacc1c20c64a5f61798241064e2
sun <sjun521@aucklanduni.ac.nz>
2021-05-07 19:59:29 +1200

    <test>: create Room test

--- frontend/src/components/__tests__/CreateRoom.test.js
+++ frontend/src/components/__tests__/CreateRoom.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, fireEvent, screen, act } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
 import CreateRoomPage from '../CreateRoom';
 import {AppContext} from '../../AppContextProvider';
 
@@ -13,5 +14,23 @@ it('creates a room properly', async() =>{
             <CreateRoomPage />
         </AppContext.Provider>
     )
-    
+    const name = screen.getByPlaceholderText('Enter room name');
+    userEvent.type(name, 'Test Room Name')
+
+    const desc = screen.getByPlaceholderText('Enter description');
+    userEvent.type(desc, 'Test description')
+
+    const pass = screen.getByPlaceholderText('Enter password');
+    userEvent.type(pass, 'Test password')
+
+    const createButton = screen.getByText('Create Room');
+    fireEvent.click(createButton);
+
+    expect(dummyCreateRoom).toHaveBeenCalledTimes(1);
+    const room = dummyCreateRoom.mock.calls[0][0];
+
+    expect(room.name).toBe('Test Room Name')
+    expect(room.description).toBe('Test description')
+    expect(room.password).toBe('Test password')
+
 })
