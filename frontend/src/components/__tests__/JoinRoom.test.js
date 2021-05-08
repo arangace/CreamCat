fee6fe12631ccd7892d7e77b3be8dac685d777d5
sun <sjun521@aucklanduni.ac.nz>
2021-05-08 15:40:09 +1200

    <feat>: join Room test update

--- frontend/src/components/__tests__/JoinRoom.test.js
+++ frontend/src/components/__tests__/JoinRoom.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, screen, act } from '@testing-library/react';
+import { render, fireEvent, screen, act, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 import JoinRoomPage from '../JoinRoom';
@@ -7,22 +7,35 @@ import {AppContext} from '../../AppContextProvider';
 
 it('handles adding to form and sending data on button click', async() =>{
     const dummyJoinRoom = jest.fn();
-    
+
     render(
         <AppContext.Provider value={{joinRoom: dummyJoinRoom}}>
             <JoinRoomPage />
         </AppContext.Provider>
     )
 
+   act(()=>{
+        const joinButton = screen.getByText('Join')
+        fireEvent.click(joinButton)
+    }) 
+
+    await waitFor(()=>screen.queryByText('Room ID is required!'))
+
+    expect(screen.queryByText('Room ID is required!'))
+    expect(dummyJoinRoom).toHaveBeenCalledTimes(0);
+    
     const name = screen.getByPlaceholderText('Room ID');
     userEvent.type(name, 'test')
 
     const desc = screen.getByPlaceholderText('Password');
     userEvent.type(desc, 'testPass')
 
-    const joinButton = screen.getByText('Join')
-    fireEvent.click(joinButton)
+    act(()=>{
+        const joinButton = screen.getByText('Join')
+        fireEvent.click(joinButton)
+    })
 
     expect(dummyJoinRoom).toHaveBeenCalledTimes(1);
+    const room = dummyJoinRoom.mock.calls[0][0];
 
 })
